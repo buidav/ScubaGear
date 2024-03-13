0b41eae0c1e1fe65ccad2eb2a657d4cdb4c29559
buidav <105074908+buidav@users.noreply.github.com>
2024-03-13 16:14:41 -0700

    r

--- PowerShell/ScubaGear/Modules/Support/Support.psm1
+++ PowerShell/ScubaGear/Modules/Support/Support.psm1
@@ -229,7 +229,7 @@ function Install-OPA {
     )
 
     # Constants
-    # The # End Versions comment is important to the OPA version bump workflow
+    # The "End Versions" comment is important to the OPA version bump workflow
     $ACCEPTABLEVERSIONS = '0.42.1','0.42.2','0.43.1','0.44.0','0.45.0','0.46.3','0.47.4','0.48.0','0.49.2','0.50.2',
     '0.51.0','0.52.0','0.53.1','0.54.0','0.55.0','0.56.0','0.57.1','0.58.0','0.59.0' # End Versions
     $FILENAME = @{ Windows = "opa_windows_amd64.exe"; MacOS = "opa_darwin_amd64"; Linux = "opa_linux_amd64_static"}
