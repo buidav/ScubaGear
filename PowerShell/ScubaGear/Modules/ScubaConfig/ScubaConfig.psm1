0e40285b9cafdd5830abbe64134c5119d6de7ef6
GitHub Action <action@github.com>
2024-03-14 21:29:36 +0000

    Bump OPA version from v0.61.0 to  v0.62.1

--- PowerShell/ScubaGear/Modules/ScubaConfig/ScubaConfig.psm1
+++ PowerShell/ScubaGear/Modules/ScubaConfig/ScubaConfig.psm1
@@ -38,7 +38,7 @@ class ScubaConfig {
             "Hybrid Identity Administrator",
             "Application Administrator",
             "Cloud Application Administrator")
-        DefaultOPAVersion = '0.61.0'
+        DefaultOPAVersion = '
     }
 
     static [object]ScubaDefault ([string]$Name){
@@ -140,3 +140,106 @@ class ScubaConfig {
         return [ScubaConfig]::_Instance
     }
 }
+
+    }
+
+    static [object]ScubaDefault ([string]$Name){
+        return [ScubaConfig]::ScubaDefaults[$Name]
+    }
+
+    [Boolean]LoadConfig([System.IO.FileInfo]$Path){
+        if (-Not (Test-Path -PathType Leaf $Path)){
+            throw [System.IO.FileNotFoundException]"Failed to load: $Path"
+        }
+        [ScubaConfig]::ResetInstance()
+        $Content = Get-Content -Raw -Path $Path
+        $this.Configuration = $Content | ConvertFrom-Yaml
+
+        $this.SetParameterDefaults()
+        [ScubaConfig]::_IsLoaded = $true
+
+        return [ScubaConfig]::_IsLoaded
+    }
+
+    hidden [void]ClearConfiguration(){
+        $this.Configuration = $null
+    }
+
+    hidden [Guid]$Uuid = [Guid]::NewGuid()
+    hidden [hashtable]$Configuration
+
+    hidden [void]SetParameterDefaults(){
+        Write-Debug "Setting ScubaConfig default values."
+        if (-Not $this.Configuration.ProductNames){
+            $this.Configuration.ProductNames = [ScubaConfig]::ScubaDefault('DefaultProductNames')
+        }
+        else{
+            # Transform ProductNames into list of all products if it contains wildcard
+            if ($this.Configuration.ProductNames.Contains('*')){
+                $this.Configuration.ProductNames = [ScubaConfig]::ScubaDefault('AllProductNames')
+                Write-Debug "Setting ProductNames to all products because of wildcard"
+            }
+            else{
+                Write-Debug "ProductNames provided - using as is."
+                $this.Configuration.ProductNames = $this.Configuration.ProductNames | Sort-Object
+            }
+        }
+
+        if (-Not $this.Configuration.M365Environment){
+            $this.Configuration.M365Environment = [ScubaConfig]::ScubaDefault('DefaultM365Environment')
+        }
+
+        if (-Not $this.Configuration.OPAPath){
+            $this.Configuration.OPAPath = [ScubaConfig]::ScubaDefault('DefaultOPAPath')
+        }
+
+        if (-Not $this.Configuration.LogIn){
+            $this.Configuration.LogIn = [ScubaConfig]::ScubaDefault('DefaultLogIn')
+        }
+
+        if (-Not $this.Configuration.DisconnectOnExit){
+            $this.Configuration.DisconnectOnExit = $false
+        }
+
+        if (-Not $this.Configuration.OutPath){
+            $this.Configuration.OutPath = [ScubaConfig]::ScubaDefault('DefaultOutPath')
+        }
+
+        if (-Not $this.Configuration.OutFolderName){
+            $this.Configuration.OutFolderName = [ScubaConfig]::ScubaDefault('DefaultOutFolderName')
+        }
+
+        if (-Not $this.Configuration.OutProviderFileName){
+            $this.Configuration.OutProviderFileName = [ScubaConfig]::ScubaDefault('DefaultOutProviderFileName')
+        }
+
+        if (-Not $this.Configuration.OutRegoFileName){
+            $this.Configuration.OutRegoFileName = [ScubaConfig]::ScubaDefault('DefaultOutRegoFileName')
+        }
+
+        if (-Not $this.Configuration.OutReportName){
+            $this.Configuration.OutReportName = [ScubaConfig]::ScubaDefault('DefaultOutReportName')
+        }
+
+        if (-Not $this.Configuration.OutJsonFileName){
+            $this.Configuration.OutJsonFileName = [ScubaConfig]::ScubaDefault('DefaultOutJsonFileName')
+        }
+
+        return
+    }
+
+    hidden ScubaConfig(){
+    }
+
+    static [void]ResetInstance(){
+        [ScubaConfig]::_Instance.ClearConfiguration()
+        [ScubaConfig]::_IsLoaded = $false
+
+        return
+    }
+
+    static [ScubaConfig]GetInstance(){
+        return [ScubaConfig]::_Instance
+    }
+}
+
